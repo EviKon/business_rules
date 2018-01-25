aba7b62ae2d473fa68614c6b800059ce8e6ead4d
Evi-Konstantoula <Paraskevi.Konstantoula@onthebeach.co.uk>
2018-01-25 10:27:49 +0000

    Refactoring

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,19 +1,33 @@
 class FizzBuzz
-  attr_reader :number
-
   def initialize(number)
     @number = number
   end
 
   def calculator
-    if number % 3 == 0 && number % 5 == 0
+    if multiple_by_both_3_and_5
       'FizzBuzz'
-    elsif number % 3 == 0 || number.to_s.include?("3")
+    elsif multiple_or_includes_3
       'Fizz'
-    elsif number % 5 == 0 || number.to_s.include?("5")
+    elsif multiple_or_includes_5
       'Buzz'
     else
       number
     end
+  end
+
+  private
+
+  attr_reader :number
+
+  def multiple_by_both_3_and_5
+    number % 3 == 0 && number % 5 == 0
+  end
+
+  def multiple_or_includes_3
+    number % 3 == 0 || number.to_s.include?("3")
+  end
+
+  def multiple_or_includes_5
+    number % 5 == 0 || number.to_s.include?("5")
   end
 end
